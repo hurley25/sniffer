4e2705ea9c21114c8d30210ed545467ac3fa7c57
hurley25 <liuhuan1992@gmail.com>
2013-01-22 14:20:24 +0800

    增加了新的 SLOT 函数，修正了原先 Action 的 connect 指向。并且增加了新的私有函数。
    Signed-off-by: hurley25 <liuhuan1992@gmail.com>

--- mainwindow.h
+++ mainwindow.h
@@ -41,17 +41,36 @@ protected:
 	void closeEvent(QCloseEvent *event);
 
 private slots:
+	void newFile();
+    void open();
+    void save();
+    void print();
+	
+	void begin();
+	void end();
+	void findQQ();
+	void filter();
+	void set();
+
 	void about();
 
 private:
 	void createMainWeiget();		// 创建主窗口
 	void createActions();			// 创建动作
 	void createMenus();				// 创建主菜单
-	void createContextMenu();		// 创建程序界面菜单
 	void createToolBars();			// 创建工具栏
 	void createStatusBar();			// 创建状态栏
+	
+	void readSettings();			// 读取配置文件
+	void writeSettings();			// 写入配置文件
 
 	bool isToContinue();			// 询问是否继续
+
+	bool loadFile(const QString &fileName);			// 载入捕获文件
+	bool saveFile(const QString &fileName);			// 保存捕获文件
+	void setCurrentFile(const QString &fileName);	// 设置当前打开文件
+
+	QString curFile;				// 当前打开文件
 
 	QSplitter *rightSplitter;		// 右边的切分窗口
 	QSplitter *mainSplitter;		// 总体的切分窗口
@@ -83,8 +102,8 @@ private:
 
 	QAction *beginAction;			// 开始捕获
 	QAction *endAction;				// 停止捕获
-	QAction *filterAction;			// 设置过滤器
 	QAction *findQQAction;			// 捕获QQ信息
+	QAction *filterAction;			// 设置过滤器
 	QAction *setAction;				// 全局设置
 
 	QAction *aboutAction;			// 关本于程序
